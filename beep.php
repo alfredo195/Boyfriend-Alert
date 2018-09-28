40748e37e78f9c0f8e5d68864cd7c2d627b47ef6
Justin DeSimpliciis <30761811+jscd@users.noreply.github.com>
2018-09-28 09:03:50 -0400

    Remove unnecessary 'sudo' in shell_exec

--- beep.php
+++ beep.php
@@ -1,7 +1,7 @@
 <?php
 	echo $_GET['key'];
 	if($_GET['key'] == REDACTED){
-    // Run in bg so page doesn't take forever to load
-		shell_exec("sudo ./run.py >/dev/null 2>/dev/null &");
+    // Run in bg so page doesn't take entire runtime to load
+		shell_exec("./run.py >/dev/null 2>/dev/null &");
 	}
 ?>
